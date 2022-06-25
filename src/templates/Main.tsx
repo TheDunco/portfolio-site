1e030dd11f8b92f755687f447126b4d943abd3ff
TheDunco <duncvan@gmail.com>
2022-06-25 19:15:00 -0400

    Updates

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -3,36 +3,30 @@ import type { ReactNode } from 'react';
 import { AppConfig } from '@/utils/AppConfig';
 
 type IMainProps = {
-  meta: ReactNode;
-  children: ReactNode;
+    meta: ReactNode;
+    children: ReactNode;
 };
 
 export type Themes = {
-  classy: string;
-  pulse: string;
-  cherry: string;
+    classy: string;
+    cherry: string;
+    pulse: string;
 };
 
 export const themes: Themes = {
-  classy: 'classy',
-  pulse: 'pulse',
-  cherry: 'cherry',
+    classy: 'classy',
+    cherry: 'cherry',
+    pulse: 'pulse',
 };
 
 const Main = (props: IMainProps) => (
-  <div className="w-full antialiased">
-    {props.meta}
-    <div className="content flex w-full font-inter text-xl font-thin">
-      {props.children}
+    <div className="w-full antialiased">
+        {props.meta}
+        <div className="content flex w-full font-inter text-xl font-thin">
+            {props.children}
+        </div>
+
     </div>
-    <div className="border-t border-gray-300 py-8 text-center text-sm">
-      © Copyright {new Date().getFullYear()} {AppConfig.title}. Powered with{' '}
-      <span role="img" aria-label="Love">
-        ♥
-      </span>{' '}
-      by <a href="https://creativedesignsguru.com">CreativeDesignsGuru</a>
-    </div>
-  </div>
 );
 
 export { Main };
